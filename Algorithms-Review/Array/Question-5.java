1e83b35ef307a66bf5746e6197629fc5ade46d09
Davi Luna <Sdavi738@gmail.com>
2023-02-23 13:24:13 -0300

    Fixing the zero number in array and increment the Manual

--- Algorithms-Review/Array/Question-5.java
+++ Algorithms-Review/Array/Question-5.java
@@ -1,26 +1,35 @@
-//Crie um algoritmo que preencha um Array com 100 numeros inteiros aleatoriamente e imprima os
-//elementos numericos que sejam ımpares
-
 import java.util.Arrays;
 import java.util.Random;
 
-public class Question5 {
+public class q5 {
 	public static void main(String[] args) {
+		
+		//================================================================
+		int tamanho = 100;//variável que irá ser alterada(tamanho do array)
+		
+		
+		
+		
+		//====================Instanciando as variaveis zero, arrau e array impar==
+		
+		int zero=0;
+		
 		int[] array;
-		array = new int[100];
+		array = new int[tamanho];
 		
 		int[] impares;
-		impares = new int[100];
+		impares = new int[tamanho];
 		
 		Random random=new Random();
 		
-		for(int i=0; i<100;i++) {
+		//===========Preenchendo o array com valores aleatórios===============
+		for(int i=0; i<tamanho;i++) {
 			array[i]=random.nextInt(200);
 		}
 		
 		int contador =0;
 		
-		for(int i=0;i<100;i++) {
+		for(int i=0;i<tamanho;i++) {
 			
 			int valor=array[i];
 			if((valor%2)!=0) {
@@ -30,8 +39,26 @@ public class Question5 {
 			}
 			
 		}
+		//================Descobrir o tamanho do array sem os zeros===========
+		for(int i=0;i<tamanho;i++) {
+			if(impares[i]==0) {
+				zero++;
+			}
+			
+		}
+		//=========================Definindo o novo array sem o zero==========
+		int tamanho_novo_array=tamanho-zero;
+		int[] array_impar_sem_zero;
+		array_impar_sem_zero = new int[tamanho_novo_array];
+		
+		//====================Preenchendo o novo array sem o zero=============
+		for(int i=0;i<(tamanho_novo_array);i++) {
+			array_impar_sem_zero[i]=impares[i];
+		}
+		System.err.println("Toma aqui teus arrays :");
 		System.out.println("Array completo: "+Arrays.toString(array));
-		System.out.println("Array com valores primos: "+Arrays.toString(impares));
+		System.out.println("Array com valores primos: "+Arrays.toString(array_impar_sem_zero));
+		
 	}
 	
 }
