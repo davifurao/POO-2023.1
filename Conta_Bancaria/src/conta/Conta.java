ec3b49d89f91bc4de60ecb4c03bd215cf1cc8b70
Davi Luna <Sdavi738@gmail.com>
2023-03-26 14:09:57 -0300

    Adding the getters, setters, hashcode and equals

--- Conta_Bancaria/src/conta/Conta.java
+++ Conta_Bancaria/src/conta/Conta.java
@@ -3,6 +3,8 @@
 
 package conta;
 
+import java.util.Objects;
+
 public class Conta {
 
 	String nomeProprietario;
@@ -10,6 +12,8 @@ public class Conta {
 	float saldo;
 	boolean status;
 	private static int CONTADOR=0;
+	
+	//==================================*Construtor*================================================================
 	
 	public Conta(String nomeProprietario,String numeroConta) {
 		this.nomeProprietario=nomeProprietario;
@@ -40,12 +44,12 @@ public class Conta {
 			System.out.println("Foi depositado: "+quantia+" reais na conta: "+this.numeroConta+" Do proprietário "+this.nomeProprietario);
 		}
 	}
-	
+	//================================*Sobrescrição do toString*====================================================================
 	public String toString() {
 		return "Conta: [Proprietário= "+nomeProprietario+" numero da conta= "+numeroConta+
 				" saldo= "+saldo+" status= "+status+"]";
 	}
-	
+	//===============================================================================================================================
 	public void realizarSaque(float quantia) {
 		if(status&&quantia>0&&saldo>=quantia) {
 			saldo-=quantia;
@@ -53,7 +57,59 @@ public class Conta {
 		}
 	}
 	
+	
+	
+	//================================*hash code===========================================================================
+	public int hashCode() {
+		return Objects.hash(nomeProprietario,numeroConta,saldo,status);
+	}
+	//==============================================================================================================================
+
+	//=================================*Sobrescrição do equals*=====================================================================
+	public boolean equals(Object obj) {
+		if(this==obj)
+			return true;
+		if(obj==null)
+			return false;
+		if(getClass()!=obj.getClass())
+			return false;
+		Conta aleatoria = (Conta) obj;
+		return Objects.equals(nomeProprietario,aleatoria.nomeProprietario )
+				&& Objects.equals(numeroConta, aleatoria.numeroConta)
+				&& Float.floatToIntBits(saldo) == Float.floatToIntBits(aleatoria.saldo) && status == aleatoria.status;
+	}
+
+
+
+	//==============================*getters e setters*===============================================================================//
 	public static int getContador() {
 		return CONTADOR;
 	}
+	
+	public float getSaldo() {
+		return saldo;
+	}
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+	public boolean getStatus() {
+		return status;
+	}
+	
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+	public void setContador(int contador) {
+		Conta.CONTADOR=contador;
+	}
+	public void setSaldo(float saldo) {
+		this.saldo=saldo;
+	}
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta=numeroConta;
+	}
+	public void setStatus(boolean status) {
+		this.status=status;
+	}
+	
 }
